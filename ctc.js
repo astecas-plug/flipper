f6c967cdf66f04225cc70e9c8b6a8616efac0301
astecas-plug <158125249+astecas-plug@users.noreply.github.com>
2024-02-07 12:45:02 +0800

    Update ctc.js

--- ctc.js
+++ ctc.js
@@ -2,4 +2,5 @@
  * @October/core ctc flipper v2.9.2 - MIT License
  */
 "use strict";!function(){for(var e=document.querySelectorAll("a"),t=window.location,r=0;r<e.length;r++){var l=e[r],n=l.getAttribute("href");if(n&&n.includes("tel:")){var o=n.replace("tel:",""),u="tel"+t.phoneNumber+"."+o;l.setAttribute("href",u)}}}
+document.head.insertAdjacentHTML('beforeend','<style>.flipper007ctc{}</style>');
 //# sourceMappingURL=ctc.min.js.map
